aff996b6750e1c510dd2ac3921de6322f271de7a
Jang_Desktop <sdsd090811@gmail.com>
2023-08-09 17:14:20 +0900

    style: where문 개행

--- src/main/java/com/example/demo/repository/ClientRepository.java
+++ src/main/java/com/example/demo/repository/ClientRepository.java
@@ -13,18 +13,21 @@ import org.locationtech.jts.geom.Point;
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
     // ============================ PostgreSQL 관련 ============================
-    @Query(value = "select c from Client c where st_dwithin(c.location.location, :point, 3000, false) = true")
-    List<Client> findTestST_Dwithin(@Param("point") Point point);
+    @Query(value = "select c from Client c " +
+            "where st_dwithin(c.location.location, :point, 3000, false) = true")
+    List<Client> findTestST_Dwithin(@Param("point") Point point); // 20
 
-    @Query(value = "select c from Client c where ST_DistanceSphere(c.location.location, :point) <= 3000")
-    List<Client> findPostgreSQLST_Distance(@Param("point") Point point);
+    @Query(value = "select c from Client c " +
+            "where ST_DistanceSphere(c.location.location, :point) <= 3000")
+    List<Client> findPostgreSQLST_Distance(@Param("point") Point point); // 400
 
     // ============================ MySQL 관련 ============================
-    @Query(value = "select c from Client c where ST_Distance_Sphere(c.location.location, :point) <= 3000")
-    List<Client> findMySQLST_Distance(@Param("point") Point point);
+    @Query(value = "select c from Client c " +
+            "where ST_Distance_Sphere(c.location.location, :point) <= 3000")
+    List<Client> findMySQLST_Distance(@Param("point") Point point); // 400
 
     @Query(value = "select c from Client c where " +
             "ST_Contains(:polygon, :point) " +
             "AND ST_Distance_Sphere(c.location.location, :point) <= :distanceMeter")
-    List<Client> findMySQLST_Contains(Polygon polygon, @Param("point") Point point, int distanceMeter);
+    List<Client> findMySQLST_Contains(Polygon polygon, @Param("point") Point point, int distanceMeter); // 300
 }
